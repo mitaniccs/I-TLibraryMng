11220c3f3b0fa71ecb052385c76144d3e4d09b02
Yudai Mitani <mitani.ccs.202204.train@gmail.com>
2022-06-01 10:11:50 +0900

    必要クラスの追加

---
+++ src/dao/Common.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Common {
+	/**
+	 * DB connectionを取得する
+	 * @return
+	 * @throws DAOException
+	 */
+	public static Connection getConnection() throws DAOException {
+		try {
+			// JDBCドライバの登録
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// URL、ユーザ名、パスワードの設定
+			String url = "jdbc:mysql://localhost:3306/articleDB	";
+			String user = "user";
+			String pass = "user";
+
+			// データベースへの接続
+			return DriverManager.getConnection(url, user, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("接続に失敗しました。");
+		}
+	}
+
+	/**
+	 * DB connectionをcloseする
+	 * @param con
+	 * @throws SQLException
+	 */
+	public static void close(Connection con) throws SQLException {
+		if (con != null) {
+			con.close();
+			con = null;
+		}
+	}
+
+
+}
